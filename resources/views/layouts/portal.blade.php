7b86679e97ca24911d7418f239b58b031ad52230
Mikhail Protasevich <m.protasevich@smartcat.ai>
2022-07-15 13:35:37 +0300

    Перенос шаблона из разметки

---
+++ resources/views/layouts/portal.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+@include('components.head')
+<body>
+@include('components.header')
+<main>
+    @yield('main')
+</main>
+@include('components.footer')
+@include('components.javascript')
+</body>
+</html>
